7ecbf908f2ba048ab0caf101b5a447a3a8d01399
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-23 20:41:20 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+def my_each(array)
+n = 0
+  while n < array.length
+    yield
+  end 
+end
+
+my_each(collection) do |n|
+  puts n
 end
